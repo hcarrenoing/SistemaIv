4547b7379f35c16ef1487e106af1a934267b5487
hcarrenoing <carreno.hugo.alexander@gmail.com>
2024-02-05 21:36:09 -0500

    first commit

---
+++ SistemaIv/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaIv.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
